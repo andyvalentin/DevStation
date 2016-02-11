bfef2e5679851806f94c228d6c43e2f8e08321ff
Tony Chabi <anthonymadeya@gmail.com>
2016-02-11 10:01:29 -0600

    master

--- DevStation/Presentation/ngApp/controllers/jobSearchController.ts
+++ DevStation/Presentation/ngApp/controllers/jobSearchController.ts
@@ -5,15 +5,22 @@
         public searchJobs(searchTerm: string) {
             this.jobs = [];
 
-            if (searchTerm) {
+            if (searchTerm != "") {
                 this.$http.get(`/api/jobs/search/${searchTerm}`)
                     .then((response) => {
                         this.jobs = response.data;
                     });
             }
+            this.$http.get(`api/jobs/list`)
+                .then((response) => {
+                    this.jobs = response.data;
+                });
         }
         public constructor(private $http: ng.IHttpService) {
-
+            this.$http.get(`api/jobs/list`)
+                .then((response) => {
+                    this.jobs = response.data;
+                });
         }
     }
 }
