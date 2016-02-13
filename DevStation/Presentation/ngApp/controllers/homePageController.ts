1e2c4b5d08ccd6c02670ef12104013f879bcaa4c
Andy <andyvalentin01@gmail.com>
2016-02-13 10:48:51 -0600

    Register and Login Modals

---
+++ DevStation/Presentation/ngApp/controllers/homePageController.ts
@@ -0,0 +1,25 @@
+﻿namespace DevStation.Controllers {
+    export class HomePageController {
+
+        public registration() {
+            this.$uibModal.open({
+                templateUrl: 'Presentation/ngApp/views/register.html',
+                controller: DevStation.Controllers.AuthController,
+                controllerAs: 'c'
+            });
+        }
+
+        public login() {
+            this.$uibModal.open({
+                templateUrl: 'Presentation/ngApp/views/login.html',
+                controller: DevStation.Controllers.AuthController,
+                controllerAs: 'c'
+            });
+        }
+
+        constructor(private $uibModal) { }
+
+    }
+
+    angular.module('DevStation').controller('HomePageController', HomePageController)
+}
