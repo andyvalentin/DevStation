9033fe1f28f290b2e18d96b92c30d1e95ab66dbd
Tony Chabi <anthonymadeya@gmail.com>
2016-02-15 16:29:09 -0600

    devDetails

---
+++ DevStation/Presentation/ngApp/controllers/profileController.ts
@@ -0,0 +1,12 @@
+﻿namespace DevStation.Controllers {
+    export class ProfileController {
+        public user;
+
+        public constructor(private $http: ng.IHttpService, private $location: ng.ILocationService) {
+            this.$http.get(`/api/users/profile`)
+                .then((response) => {
+                    this.user = response.data;
+                })
+        }
+    }
+}
