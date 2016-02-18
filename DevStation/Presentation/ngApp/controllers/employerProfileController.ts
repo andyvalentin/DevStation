27fb00e19f6213c0e31a7b3e782b7770a700aaa1
Tony Chabi <anthonymadeya@gmail.com>
2016-02-18 10:14:06 -0600

    include missing controller

---
+++ DevStation/Presentation/ngApp/controllers/employerProfileController.ts
@@ -0,0 +1,20 @@
+﻿namespace DevStation.Controllers {
+    export class EmployerProfileController {
+        public user;
+
+        public toEdit() {
+            this.$location.path("/user/devprofile/edit")
+        }
+
+        public toAddJob() {
+            this.$location.path("/user/employer/addjob")
+        }
+
+        constructor(private $http: ng.IHttpService, private $location: ng.ILocationService) {
+            this.$http.get(`/api/users/employerProfile`)
+                .then((response) => {
+                    this.user = response.data;
+                })
+        }
+    }
+}
